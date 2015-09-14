b18937e50da121de8debd8e890326d981c14732a
David Elliott <david@gophilosophie.com>
2015-09-14 12:36:46 -0700

    adds loading icon

--- src/app/directives/validators/title_uniqueness.js
+++ src/app/directives/validators/title_uniqueness.js
@@ -3,13 +3,13 @@ function TitleUniquenessValidator ($q, $http) {
 
   var link = function($scope, element, attrs, ngModel) {
     ngModel.$asyncValidators.titleUnique = function(title) {
-      var params = JSON.stringify({ 'title' : title })
+      var params = JSON.stringify({ 'title' : title });
       return $http.get(`http://localhost:4000/posts/validate?post=${params}`).
         then(function resolved(response){
           if (response.data.errors.title) {
-            return $q.reject(response.data.errors.title[0])
+            return $q.reject(response.data.errors.title[0]);
           } else {
-            return true
+            return true;
           }
         });
     };
